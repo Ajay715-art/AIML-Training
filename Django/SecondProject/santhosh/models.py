1a458148bc92d76e6df5b959a6e3a09336abe36e
Ajay715-art <ajaypateltpatel@gmail.com>
2025-07-28 11:20:14 +0530

    uptoday

--- Django/SecondProject/santhosh/models.py
+++ Django/SecondProject/santhosh/models.py
@@ -4,6 +4,4 @@ from django.db import models
 class Post(models.Model):
     name=models.CharField(max_length=100)
     roll=models.IntegerField()
-    email=models.EmailField(null=True,blank=True)
-    def __str__(self):
-        return self.name
+    
